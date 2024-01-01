e01a41efff89320dfe27157855fd6f0c13d0b5a1
Craig Puxty <craig_puxty@talktalk.net>
2024-01-01 21:07:19 +0000

    Installed redux-devtools-extension, updated store.

--- src/store.js
+++ src/store.js
@@ -4,6 +4,9 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 // Redux thunk
 import { thunk } from 'redux-thunk';
 
+// Redux dev-tools
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 // Reducers
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
@@ -13,6 +16,9 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
