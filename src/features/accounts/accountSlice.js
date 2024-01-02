3b4f86c1116c626223dc2f3290f9ab37a2254d91
Craig Puxty <craig_puxty@talktalk.net>
2024-01-02 18:55:03 +0000

    Updated accountSlice with async deposit action (thunk).

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -14,6 +14,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -30,15 +31,35 @@ const accountSlice = createSlice({
         state.balance += action.payload.amount;
       },
     },
-
-    payLoan(state, action) {
+    payLoan(state) {
       state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = '';
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+  return async function (dispatch, getState) {
+    try {
+      dispatch({ type: 'account/convertingCurrency' });
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+      );
+      if (!res.ok) throw new Error('Oops! Something went wrong?');
+      const data = await res.json();
+      const converted = data.rates.USD;
+      dispatch({ type: 'account/deposit', payload: converted });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}
 
 export default accountSlice.reducer;
